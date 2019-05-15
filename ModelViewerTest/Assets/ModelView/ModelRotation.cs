aa55e40cd469e0a213041c44e5dcbe3114c05197
tricore-as03 <tricore.as03@gmail.com>
2019-05-15 13:04:13 +0900

    該当箇所の修正 ・余分な改行箇所とコメントを削除した。 ・コスト削減のため、Start関数を削除して、Vector2.zeroで初期化を行った。 ・スワイプ操作に関するコードで前回のタップ位置の差分を取得する変数がUnityに標準で用意されていたため、自分で追加していた差分に関するコードを全て削除した。

--- ModelViewerTest/Assets/ModelView/ModelRotation.cs
+++ ModelViewerTest/Assets/ModelView/ModelRotation.cs
@@ -7,21 +7,10 @@ using UnityEngine;
 /// </summary>
 public class ModelRotation : MonoBehaviour
 {
-    Vector2 currentTouchPos;                    // 現在のタッチ位置の座標
-    Vector2 prevTouchPos;                       // 前フレームのタッチ位置の座標
+    Vector2 currentTouchPos = Vector2.zero;     // 現在のタッチ位置の座標
+    Vector2 prevTouchPos    = Vector2.zero;     // 前フレームのタッチ位置の座標]
 
     const float RotationSensitivity = 0.5f;     // モデルの回転の感度
-
-
-    /// <summary>
-    /// 開始
-    /// </summary>
-    void Start()
-    {
-        // 変数の初期化
-        currentTouchPos = new Vector2(0, 0);
-        prevTouchPos    = new Vector2(0, 0);
-    }
 
     /// <summary>
     /// 更新
@@ -34,15 +23,10 @@ public class ModelRotation : MonoBehaviour
         // スワイプ中の処理
         if (touchInfo.phase == TouchPhase.Moved)
         {
-            // 前フレームのタッチ位置の座標をセット
-            prevTouchPos = currentTouchPos;
-            // 現在のタッチ位置の座標をセット
-            currentTouchPos = touchInfo.position;
-
             // スワイプの方向を算出
-            Vector2 swipeDir = (currentTouchPos - prevTouchPos).normalized;
+            Vector2 swipeDir = touchInfo.deltaPosition.normalized;
             // 前フレームからのスワイプの移動量を算出
-            float swipeMoveAmount = (currentTouchPos - prevTouchPos).magnitude;
+            float swipeMoveAmount = touchInfo.deltaPosition.magnitude;
 
             // スワイプの向きと移動量をもとにモデルを回転させる
             gameObject.transform.Rotate(new Vector3(swipeDir.y,-swipeDir.x, 0) * (swipeMoveAmount * RotationSensitivity), Space.World);
