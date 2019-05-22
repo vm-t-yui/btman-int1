3c075bdde3b67278d51b6fad6427c9cd51c7d55f
tricore-05 <tricore.as05@gamil.com>
2019-05-22 14:12:38 +0900

    GameCenterのログイン、実績、リーダーボードの導入です。

---
+++ GameCenterTest/Assets/GameCenterManager.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SocialPlatforms;
+using System.Collections;
+
+/// <summary>
+/// GameCenterの管理クラス
+/// </summary>
+public class GameCenterManager : MonoBehaviour
+{
+    [SerializeField] InputField inputField;             //リーダーボードの点数入力用のInputField
+
+    private const string leaderBoardId = "maxdistance"; // リーダボードID 
+    private const string achievementID = "firstjump";   // 達成項目ID
+
+    //プラットフxームがUnityの時のみ使用可能
+#if UNITY_IPHONE
+
+    void Awake()
+    {
+        // 初期化処理
+        Social.localUser.Authenticate(processAuthentication);
+    }
+
+    /// <summary>
+    /// 認証に成功したかどうか
+    /// </summary>
+    /// <param name="success">If set to <c>true</c> success.</param>
+    private void processAuthentication(bool success)
+    {
+        if (success)
+        {
+            // 認証が成功した時の処理
+        }
+        else
+        {
+            // 認証が失敗した時の処理
+        }
+    }
+
+    /// <summary>
+    /// リーダーボード表示　Inspector上のLeaderBoardを押すと反応
+    /// </summary>
+    public void ShowLeaderBoard()
+    {
+        Social.ShowLeaderboardUI();
+    }
+
+    /// <summary>
+    /// リーダーボード用のハイスコア送信　Inspector上のScoreButtonを押すと反応
+    /// </summary>
+    public void SendLeaderboardScore()
+    {
+        int score = 0;
+        if (int.TryParse(inputField.text, out score))
+        {
+            Social.ReportScore(score, leaderBoardId, success => {
+                if (success)
+                {
+                    // 送信が成功した時の処理
+                }
+                else
+                {
+                    // 送信が失敗した時の処理
+                }
+            });
+        }
+    }
+
+    /// <summary>
+    /// 達成項目の表示　Inspector上のAchievementsを押すと反応
+    /// </summary>
+    public static void ShowAchievementsUI()
+    {
+        Social.ShowAchievementsUI();
+    }
+
+    /// <summary>
+    /// 達成項目用のポイント送信　Inspector上のSendPointを押すと反応
+    /// </summary>
+    public void SendAchievementsPoint()
+    {
+        Social.ReportProgress(achievementID, 100.0, success => {
+
+            //送信が成功した時の処理
+        });
+    }
+#endif
+}
