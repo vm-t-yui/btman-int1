b43ebcc301c6f80474ab07694ca64ac8ef625aad
TanakaMasaki <0sv65728326399x@ezweb.ne.jp>
2019-05-23 18:11:26 +0900

    指摘箇所修正、実績IDの定数化

--- GooglePlayGameTest/Assets/GoogleAchievementManager.cs
+++ GooglePlayGameTest/Assets/GoogleAchievementManager.cs
@@ -18,7 +18,13 @@ public class GoogleAchievementManager : MonoBehaviour
 
     const int AchievementNum = 5;                            // 実績の総数
 
-    string[] achievementIDs = new string[AchievementNum];    // 実績ID群
+    readonly string[] AchievementIDs = {                     // 実績ID群
+        "CgkI_vDQ3ZYdEAIQAQ",                                // 実績１：初ログイン
+        "CgkI_vDQ3ZYdEAIQAg",                                // 実績２：タップ 
+        "CgkI_vDQ3ZYdEAIQAw",                                // 実績３：2本指タップ 
+        "CgkI_vDQ3ZYdEAIQBA",                                // 実績４：3本指タップ 
+        "CgkI_vDQ3ZYdEAIQBQ"                                 // 実績５：4本指タップ 
+    };
 
     bool[] isReleased = new bool[AchievementNum];            // 実績解除状況
 
@@ -30,11 +36,8 @@ public class GoogleAchievementManager : MonoBehaviour
         // GoogleService機能管理クラス取得
         googleService = googleServiceObj.GetComponent<GoogleServiceManager>();
 
-        // 実績IDをセット
-        SetAchievementIDs();
-
         // 実績解除状況初期化
-        for (int i = 0; i < 5; i++)
+        for (int i = 0; i < AchievementNum; i++)
         {
             isReleased[i] = false;
         }
@@ -48,35 +51,35 @@ public class GoogleAchievementManager : MonoBehaviour
         // ログインに成功したら実績１解除
         if (googleService.IsSignIn && !isReleased[0])
         {
-            ReleaseAchievement(achievementIDs[0], 100.0f);
+            ReleaseAchievement(AchievementIDs[0], 100.0f);
             isReleased[0] = true;
         }
 
         // タップしたら実績２解除
         if (Input.touchCount >= 1 && !isReleased[1])
         {
-            ReleaseAchievement(achievementIDs[1], 100.0f);
+            ReleaseAchievement(AchievementIDs[1], 100.0f);
             isReleased[1] = true;
         }
 
         // 2本指でタップしたら実績３解除
         if (Input.touchCount >= 2 && !isReleased[2])
         {
-            ReleaseAchievement(achievementIDs[2], 100.0f);
+            ReleaseAchievement(AchievementIDs[2], 100.0f);
             isReleased[2] = true;
         }
 
         // 3本指でタップしたら実績４解除
         if (Input.touchCount >= 3 && !isReleased[3])
         {
-            ReleaseAchievement(achievementIDs[3], 100.0f);
+            ReleaseAchievement(AchievementIDs[3], 100.0f);
             isReleased[3] = true;
         }
 
         // 4本指でタップしたら実績５解除
         if (Input.touchCount >= 4 && !isReleased[4])
         {
-            ReleaseAchievement(achievementIDs[4], 100.0f);
+            ReleaseAchievement(AchievementIDs[4], 100.0f);
             isReleased[4] = true;
         }
     }
@@ -102,17 +105,5 @@ public class GoogleAchievementManager : MonoBehaviour
     public void ShowAchievements()
     {
         Social.ShowAchievementsUI();
-    }
-
-    /// <summary>
-    /// 実績IDセット
-    /// </summary>
-    void SetAchievementIDs()
-    {
-        achievementIDs[0] = "CgkI_vDQ3ZYdEAIQAQ";    // 実績１：初ログイン
-        achievementIDs[1] = "CgkI_vDQ3ZYdEAIQAg";    // 実績２：タップ
-        achievementIDs[2] = "CgkI_vDQ3ZYdEAIQAw";    // 実績３：2本指タップ
-        achievementIDs[3] = "CgkI_vDQ3ZYdEAIQBA";    // 実績４：3本指タップ
-        achievementIDs[4] = "CgkI_vDQ3ZYdEAIQBQ";    // 実績５：4本指タップ
     }
 }
