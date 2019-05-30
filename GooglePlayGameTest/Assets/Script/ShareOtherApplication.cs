3b2f4366b118d72813afe3df6cf514078cddd73e
TanakaMasaki <0sv65728326399x@ezweb.ne.jp>
2019-05-30 10:14:07 +0900

    共有機能実装

---
+++ GooglePlayGameTest/Assets/Script/ShareOtherApplication.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 共有機能クラス
+/// </summary>
+public class ShareOtherApplication : MonoBehaviour
+{
+    /// <summary>
+    /// 共有処理（共有先のアプリを選択するウィンドウが表示される）
+    /// </summary>
+    public void Share()
+    {
+        // Share("送るテキスト", "URL", "画像のURL")
+        SocialConnector.SocialConnector.Share("ShareTest");
+    }
+}
