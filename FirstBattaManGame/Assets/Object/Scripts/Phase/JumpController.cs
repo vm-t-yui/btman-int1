09f82ef90ad0989bef41f3413f1589cd6b613141
tricore-as03 <tricore.as03@gmail.com>
2019-05-31 18:01:28 +0900

    フェーズまわりのリファクタリング ・フェーズのステートまわりの仕様を大幅に変更した。 ・今まで残していた、ScenePhase.csを削除した。
    ジャンプ力を溜めて、ジャンプしてジャンプ力が無くなったら停止するところまでの流れができています。
    ゲームの仕様をまとめた結果、リザルト画面は別のシーンで行うことになったためフェーズでのリザルト処理は削除した。
    今後は、PhaseStateクラスの関数群が多くなったのでその対策を考える。
    
    【確認ファイル】
    ChargeCountDown.cs
    JumpController.cs
    JumpHeightCounter.cs
    PhaseState.cs

--- FirstBattaManGame/Assets/Object/Scripts/Phase/JumpController.cs
+++ FirstBattaManGame/Assets/Object/Scripts/Phase/JumpController.cs
@@ -12,35 +12,46 @@ public class JumpController : MonoBehaviour
     // ジャンプ中どうか
     public bool IsJumping { get; private set; } = false;
 
+    // ワンタップあたりのジャンプ力
+    const float OneTouchJumpPower = 5;
+
     /// <summary>
     /// プレイヤーをジャンプさせる
     /// </summary>
-    /// <param name="power">ジャンプ力</param>
-    public void PlayerJump(float power)
+    /// <param name="touchCount">タッチのカウント数</param>
+    public void PlayerJump(int touchCount)
     {
-        // プレイヤーに上方向に力を加える
-        playerRigidbody.AddForce(Vector3.up * power, ForceMode.VelocityChange);
+        // プレイヤーに上方向に力を加える（ジャンプ力＝ワンタップあたりのジャンプ力＊タッチされた数）
+        playerRigidbody.AddForce(Vector3.up * (OneTouchJumpPower * touchCount), ForceMode.VelocityChange);
         // ジャンプフラグを立てる
         IsJumping = true;
     }
 
     /// <summary>
-    /// ジャンプの状態を監視する
+    /// 更新
     /// </summary>
-    /// <returns>ジャンプ中かどうかを返す</returns>
-    public void ObserverJumpState()
+    void Update()
     {
         // ジャンプ中の処理
         if (IsJumping)
         {
             // ベロシティが下向きになったら
-            if (playerRigidbody.velocity.y <= 0)
+            if (playerRigidbody.velocity.y < 0)
             {
-                // ジャンプフラグを倒す
-                IsJumping = false;
-                // プレイヤーの重力を停止する
-                playerRigidbody.useGravity = false;
+                // コールバック関数を呼ぶ
+                OnJumpStop();
             }
         }
+    }
+
+    /// <summary>
+    /// ジャンプが停止した場合のコールバック
+    /// </summary>
+    public void OnJumpStop()
+    {
+        // ジャンプフラグを倒す
+        IsJumping = false;
+        // プレイヤーの物理演算を停止する
+        playerRigidbody.isKinematic = true;
     }
 }
--- FirstBattaManGame/Assets/Object/Scripts/Phase/PhaseState.cs
+++ FirstBattaManGame/Assets/Object/Scripts/Phase/PhaseState.cs
@@ -5,30 +5,35 @@ using UnityEngine;
 /// <summary>
 /// フェーズの状態を管理するクラス
 /// </summary>
-public class PhaseState : MonoBehaviour
+public partial class PhaseState : MonoBehaviour
 {
     /// <summary>
     /// フェーズの種類
     /// </summary>
     public enum PhaseType
     {
-        ChargeCountDown,    // ジャンプ力を溜めている際のカウントダウン
-        PlayerJump,         // ジャンプ
+        ChargeCountDown,       // ジャンプ力を溜めている際のカウントダウン
+        JumpHeightCounter,     // プレイヤーのジャンプ高さを計測するクラス
     }
 
-    [SerializeField] PhaseChargeCountDown phaseChargeJumpPower = default;       // フェーズ：ジャンプ力を溜める
     // ステートマシン
-    static public StateMachine<PhaseType> StateMachine { get; private set; } = new StateMachine<PhaseType>();
+    StateMachine<PhaseType> stateMachine = new StateMachine<PhaseType>();
+
+    [SerializeField] ChargeCountDown   chargeCountDown   = default;         // ジャンプ力の溜めている際のカウントダウン
+    [SerializeField] JumpHeightCounter jumpHeightCounter = default;         // プレイヤーのジャンプ高さを計測するクラス
 
     /// <summary>
     /// 開始
     /// </summary>
     void Start()
     {
-        // フェーズのそれぞれの関数をステートマシンに追加
-        StateMachine.Add(PhaseType.ChargeCountDown, phaseChargeJumpPower.Initializer, phaseChargeJumpPower.Updater, phaseChargeJumpPower.Cleanup);
-        // 最初のフェーズをセット
-        StateMachine.SetState(PhaseType.ChargeCountDown);
+        // "ChargeCountDown"の関数をステートマシンに追加
+        stateMachine.Add(PhaseType.ChargeCountDown, EnterChargeCountDown, UpdateChargeCountDown, ExitChargeCountDown);
+        // "JumpHeightCounter"の関数をステートマシンに追加
+        stateMachine.Add(PhaseType.JumpHeightCounter, EnterJumpHeightCounter, UpdateJumpHeightCounter, ExitJumpHeightCounter);
+
+        // 最初のステートを"ChargeCountDown"にセット
+        stateMachine.SetState(PhaseType.ChargeCountDown);
     }
 
     /// <summary>
@@ -38,8 +43,62 @@ public class PhaseState : MonoBehaviour
     void FixedUpdate()
     {
         // 各フェーズの更新を行う
-        StateMachine.Update();
+        stateMachine.Update();
     }
 
+    /// <summary>
+    /// "ChargeCountDown"の初期化処理
+    /// </summary>
+    void EnterChargeCountDown()
+    {
+        // "ChargeCountDown"をtrueに設定
+        chargeCountDown.gameObject.SetActive(true);
+    }
 
+    /// <summary>
+    /// "ChargeCountDown"の更新処理
+    /// </summary>
+    void UpdateChargeCountDown()
+    {
+        // カウントダウンの値が０になったら
+        if (chargeCountDown.CurrentCountNum <= 0.1f)
+        {
+            // ステートを"JumpHeightCounter"に変更する
+            stateMachine.SetState(PhaseType.JumpHeightCounter);
+        }
+    }
+
+    /// <summary>
+    /// "ChargeCountDown"の終了処理
+    /// </summary>
+    void ExitChargeCountDown()
+    {
+        // "ChargeCountDown"をfalseに設定
+        chargeCountDown.gameObject.SetActive(false);
+    }
+
+    /// <summary>
+    /// "JumpHeightCounter"の初期化処理
+    /// </summary>
+    void EnterJumpHeightCounter()
+    {
+        // "JumpHeightCounter"をtrueに設定
+        jumpHeightCounter.gameObject.SetActive(true);
+    }
+
+    /// <summary>
+    /// "JumpHeightCounter"の更新処理
+    /// </summary>
+    /// TODO：t.mitsumaru（未実装今後処理を追加する可能性があるため関数のみを追加）
+    void UpdateJumpHeightCounter()
+    {
+    }
+
+    /// <summary>
+    /// "JumpHeightCounter"の終了処理
+    /// </summary>
+    /// TODO：t.mitsumaru（未実装今後処理を追加する可能性があるため関数のみを追加）
+    void ExitJumpHeightCounter()
+    {
+    }
 }
