dd24eada44d0fe192040984ab2af5191699cb35d
tricore.as03 <tricore.as03@gmail.com>
2019-06-10 18:32:16 +0900

    オーディオ再生システムの調整
    ・大石君から受け取ったAudioManagerのリファクタリングを行った。
    　┗ クラスの名前を「AudioPlayer」に変更した。
    　┗ Bgmのパラメータを持つクラス「BgmParameter」を作成した。
    　┗ Seのパラメータを持つクラス「SeParameter」を作成した。
    　（それぞれのパラメータのクラスを分けた理由は、オプション側のデータのやり取りや今後の機能拡張が容易に行えると考えたため）
    
    【確認ファイル】
    AudioPlayer.cs
    BgmParameter.cs
    SeParameter.cs
    
    「オーディオ再生システムの現在の流れについて」
    SEでは再生を開始するたびにオブジェクトを複製して、専用の親オブジェクトの子に登録する。
    現段階では再生が終了したSEの子オブジェクトが残ったままになっている。
    なので今後、残っているオブジェクトをどこかのタイミングで削除する処理を追加する必要がある。
    ただ、再生が終了した段階で削除を行うと確保と削除の繰り返しで断片化が起きてしまう可能性があるので、イメージではオブジェクトの数が指定の数を超えたら一斉に削除するような処理を別クラスで追加しようと考えている。
    ちなみに、BGMは一度にひとつしか再生されないことが決まっているのでSetActiveで切り替える。

---
+++ FirstBattaManGame/Assets/Object/Scripts/Audio/BgmParameter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// BGMのパラメータクラス
+/// </summary>
+public class BgmParameter : MonoBehaviour
+{
+    // AudioSourceコンポーネント
+    [SerializeField] AudioSource bgmAudioSource = default;
+
+    /// <summary>
+    /// 更新
+    /// </summary>
+    void Update()
+    {
+        // 再生が終了したBGMはオブジェクトを切る
+        if (!bgmAudioSource.isPlaying)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ FirstBattaManGame/Assets/Object/Scripts/Audio/SeParameter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// SEのパラメータクラス
+/// </summary>
+public class SeParameter : MonoBehaviour
+{
+    // AudioSourceコンポーネント
+    [SerializeField] AudioSource seAudioSource = default;
+
+    /// <summary>
+    /// 更新
+    /// </summary>
+    void Update()
+    {
+        // 再生が終了したSEはオブジェクトを切る
+        if (!seAudioSource.isPlaying)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
