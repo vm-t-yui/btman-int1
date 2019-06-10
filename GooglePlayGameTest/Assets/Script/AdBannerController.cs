5ba35d10be4a70e4bbe879f807281002e66dacc2
TanakaMasaki <0sv65728326399x@ezweb.ne.jp>
2019-06-10 22:17:29 +0900

    コミット漏れ

--- GooglePlayGameTest/Assets/Script/AdBannerController.cs
+++ GooglePlayGameTest/Assets/Script/AdBannerController.cs
@@ -13,6 +13,8 @@ public class AdBannerController : MonoBehaviour
 
     const string AdUnitId = "ca-app-pub-3940256099942544/6300978111";   // 広告ユニットID（テスト用ID）
 
+    public bool IsLoaded { get; private set; } = false;                 // ロード完了フラグ
+
     /// <summary>
     /// バナー広告生成
     /// </summary>
@@ -29,6 +31,8 @@ public class AdBannerController : MonoBehaviour
 
         // 表示状態で生成されるので非表示にする
         bannerView.Hide();
+
+        IsLoaded = true;
     }
 
     /// <summary>
