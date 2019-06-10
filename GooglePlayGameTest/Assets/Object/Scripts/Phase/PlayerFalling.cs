323380b9cd45c39093bfa852848a237c36a1b825
TanakaMasaki <0sv65728326399x@ezweb.ne.jp>
2019-06-10 18:30:29 +0900

    三丸作成のメインゲーム部分追加

---
+++ GooglePlayGameTest/Assets/Object/Scripts/Phase/PlayerFalling.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ジャンプ後の落下処理
+/// </summary>
+public class PlayerFalling : MonoBehaviour
+{
+    // プレイヤーのリジッドボディ
+    [SerializeField] Rigidbody playerRigidbody = default;
+
+    // 現在の待機時間
+    int currentFallWaitTimeCount = 0;
+    // 落下するまでの待機時間
+    const int FallWaitTime = 60;
+    // 落下フラグ
+    bool isFalling = false;
+    // 落下中のカウント
+    float fallingTime = 0;
+    // 落下させる時間
+    const float FallingMaxTime = 2;
+    // 処理終了フラグ
+    public bool IsEnd { get; private set; } = false;
+
+    /// <summary>
+    /// 更新処理
+    /// </summary>
+    void Update()
+    {
+        currentFallWaitTimeCount++;
+        // 指定の時間まで待機したら、プレイヤーの物理演算をオンにして落下させる
+        if (currentFallWaitTimeCount > FallWaitTime)
+        {
+            // 物理演算をオンにする
+            playerRigidbody.isKinematic = false;
+            // 落下フラグを起こす
+            isFalling = true;
+        }
+
+        // 落下が始まったらカウントを回す
+        if (isFalling)
+        {
+            fallingTime += Time.deltaTime;
+
+            if (fallingTime >= FallingMaxTime)
+            {
+                IsEnd = true;
+            }
+        }
+    }
+}
