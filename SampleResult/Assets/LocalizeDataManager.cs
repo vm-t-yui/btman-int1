ea9aae01c32047bc8852e7d8c0fcf369340e6fe5
tricore-05 <tricore.as05@gamil.com>
2019-06-11 11:08:54 +0900

    コミット追加

---
+++ SampleResult/Assets/LocalizeDataManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocalizeDataManager : MonoBehaviour
+{
+    //言語
+    enum LocalizeLanguage
+    {
+        None = -1,  //データ無し
+        Japanese,   //日本語
+        English,    //英語
+        German,     //ドイツ語
+        Italian,    //イタリア語
+        French,     //フランス語
+        Chinese,    //中国語
+        Spanish,    //スペイン語
+
+    }
+
+    [SerializeField]
+    LocalizeController localizeController;  //スコアクラス
+
+    int languageNum = 0;                    //言語の番号
+
+    /// <summary>
+    /// 起動時処理
+    /// </summary>
+    void Awake()
+    {
+        //データロード
+        LoadData();
+    }
+
+    /// <summary>
+    /// データロード
+    /// </summary>
+    public void LoadData()
+    {
+        //データロード
+        languageNum = PlayerPrefs.GetInt("Language", -1);
+
+        //ロードしたデータをセット
+        localizeController.SetLanguageNum(languageNum);
+    }
+
+    /// <summary>
+    /// データセーブ
+    /// </summary>
+    public void SaveData()
+    {
+        //データを取得してセット
+        languageNum = localizeController.GetLanguageNum();
+        PlayerPrefs.SetInt("Language", languageNum);
+
+        //セットしたデータをセーブ
+        PlayerPrefs.Save();
+    }
+
+    /// <summary>
+    /// セーブデータ消去
+    /// </summary>
+    public void DeleteData()
+    {
+        PlayerPrefs.DeleteKey("Language");
+    }
+}
