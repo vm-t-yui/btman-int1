b09783f73714af0dc1f6fa4b6ee575b66b8b730e
tricore.as03 <tricore.as03@gmail.com>
2019-06-11 15:55:35 +0900

    再生が終了したSEのオブジェクトの後処理
    ・再生が終了したSEのオブジェクトが残ってしまっていたため、その削除を行った。
    　┗ 終了したSEを専用の親オブジェクトに切り替えるクラス「PlayingSeParentSwitcher」を作成した。
    　┗ 溜まったSEのオブジェクトを一斉に削除するクラス「EndedSeDestroyer」を作成した。
    
    【確認ファイル】
    ・PlayingSeParentSwitcher.cs
    ・EndedSeDestroyer.cs

---
+++ FirstBattaManGame/Assets/Object/Scripts/Audio/PlayingSeParentSwitcher.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 再生中のSEのオブジェクトを監視して、親オブジェクトの切り替えを行う
+/// </summary>
+public class PlayingSeParentSwitcher : MonoBehaviour
+{
+    // 再生が終了しているSEの親オブジェクトのトランスフォーム
+    [SerializeField] Transform parentEndSe = default;
+
+    /// <summary>
+    /// 更新
+    /// </summary>
+    void Update()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            // 子オブジェクトを取得
+            GameObject childSe = transform.GetChild(i).gameObject;
+
+            // 子オブジェクトの中で既に再生が終了しているSEは専用の親オブジェクトに切り替える
+            if (!childSe.activeSelf)
+            {
+                childSe.transform.SetParent(parentEndSe);
+            }
+        }
+    }
+}
