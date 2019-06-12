80f93257894c78db8799679feea349e5840a794c
tricore.as03 <tricore.as03@gmail.com>
2019-06-12 11:12:28 +0900

    Re-再生が終了したSEのオブジェクトの後処理
    再生が終了したSEのオブジェクトが残ってしまっていたため、その削除を行った。
    ┗ 終了したSEを専用の親オブジェクトに切り替えるクラス「PlayingSeParentSwitcher」を作成した。
    ┗ 溜まったSEのオブジェクトを一斉に削除するクラス「EndedSeDestroyer」を作成した。
    
    【確認ファイル】
    ・PlayingSeParentSwitcher.cs
    ・EndedSeDestroyer.cs

---
+++ BattaJump/Assets/Script/Audio/EndedSeDestroyer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 再生が終了したSEオブジェクトの削除処理
+/// </summary>
+public class EndedSeDestroyer : MonoBehaviour
+{
+    // 子オブジェクトの最大数
+    [SerializeField] int childAmountMax = 0;
+
+    /// <summary>
+    /// 更新
+    /// </summary>
+    void Update()
+    {
+        // 子オブジェクトの最大数を超えたら
+        if (transform.childCount == childAmountMax)
+        {
+            // 再生が終了したSEの子オブジェクトを一斉に削除する
+            foreach (Transform endedSeChild in transform)
+            {
+                Destroy(endedSeChild.gameObject);
+            }
+        }
+    }
+}
