8ac8f1414136128975e4241d25f09e508b55a08b
tricore.as03 <tricore.as03@gmail.com>
2019-06-12 15:03:40 +0900

    オーディオのパラメータの管理
    オプションとのパラメータのやり取りのため、パラメータを管理する処理を作成
    ┗ BGMとSEのそれぞれでパラメータ専用のクラスを作成
    （パラメータ専用のクラスを介してオプションとやり取りを行う）
    
    SoundPlayTestクラスはサウンドの再生をテストするだけのクラスなのでゲーム内容とは無関係
    スクリプトをアタッチしているオブジェクトにもEditorOnlyタグが付いているので、エディタのみで動作する。
    
    【確認ファイル】
    ・BgmParameter.cs
    ・BgmAudioSource.cs
    
    ・SeParameter.cs
    ・SeAudioSource.cs
    
    ・SoundPlayTest.cs

--- BattaJump/Assets/Script/Audio/SeAudioSource.cs
+++ BattaJump/Assets/Script/Audio/SeAudioSource.cs
@@ -10,11 +10,18 @@ public class SeAudioSource : MonoBehaviour
     // AudioSourceコンポーネント
     [SerializeField] AudioSource audioSource = default;
 
+    // SEのパラメータ
+    [SerializeField] SeParameter seParameter = default;
+
     /// <summary>
     /// 更新
     /// </summary>
     void Update()
     {
+        // SEのパラメータをセットする
+        audioSource.volume = seParameter.GetPlayVolume();
+        audioSource.mute   = seParameter.GetMuteFlag();
+
         // 再生が終了したSEはオブジェクトを切る
         if (!audioSource.isPlaying)
         {
