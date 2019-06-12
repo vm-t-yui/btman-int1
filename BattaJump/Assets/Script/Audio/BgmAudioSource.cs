1d94a0f010ba91d5a4332cee45bda15bd0670e2e
tricore.as03 <tricore.as03@gmail.com>
2019-06-12 16:16:16 +0900

    Revert "オーディオのパラメータの管理"
    This reverts commit 8ac8f1414136128975e4241d25f09e508b55a08b.

--- BattaJump/Assets/Script/Audio/BgmAudioSource.cs
+++ BattaJump/Assets/Script/Audio/BgmAudioSource.cs
@@ -9,18 +9,12 @@ public class BgmAudioSource: MonoBehaviour
 {
     // AudioSourceコンポーネント
     [SerializeField] AudioSource audioSource = default;
-    // BGMのパラメータ
-    [SerializeField] BgmParameter bgmParameter = default;
 
     /// <summary>
     /// 更新
     /// </summary>
     void Update()
     {
-        // BGMのパラメータをセットする
-        audioSource.volume = bgmParameter.GetPlayVolume();
-        audioSource.mute   = bgmParameter.GetMuteFlag();
-
         // 再生が終了したBGMはオブジェクトを切る
         if (!audioSource.isPlaying)
         {
