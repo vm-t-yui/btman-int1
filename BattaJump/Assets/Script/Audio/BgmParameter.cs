d2faf8089e6a12e68f4f2641c17955f190853549
tricore.as03 <tricore.as03@gmail.com>
2019-06-12 17:54:36 +0900

    オーディオパラメータの管理 オプションとのパラメータのやり取りのため、パラメータを管理する処理を作成 ┗ BGMとSEのそれぞれでパラメータ専用のクラスを作成 （パラメータ専用のクラスを介してオプションとやり取りを行う）
    SoundPlayTestクラスはサウンドの再生をテストするだけのクラスなのでゲーム内容とは無関係
    スクリプトをアタッチしているオブジェクトにもEditorOnlyタグが付いているので、エディタのみで動作する。
    
    【確認ファイル】
    ・BgmParameter.cs
    ・BgmAudioSource.cs
    
    ・SeParameter.cs
    ・SeAudioSource.cs
    
    ・SoundPlayTest.cs

---
+++ BattaJump/Assets/Script/Audio/BgmParameter.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// BGM共通のパラメータ
+/// </summary>
+[CreateAssetMenu]
+public class BgmParameter : ScriptableObject
+{
+    // BGMの再生ボリューム
+    [SerializeField][Range(0,1)]
+    float playVolumeBgm = 1;
+
+    // BGMがミュートかどうか
+    [SerializeField]
+    bool isMuteBgm = false;
+
+    /// <summary>
+    /// 再生ボリュームを取得
+    /// </summary>
+    /// <returns>現在のボリュームを返す</returns>
+    public float GetPlayVolume()
+    {
+        return playVolumeBgm;
+    }
+
+    /// <summary>
+    /// 再生ボリュームをセット
+    /// </summary>
+    /// <param name="set">セットする値</param>
+    public void SetPlayVolume(float set)
+    {
+        playVolumeBgm = set;
+    }
+
+    /// <summary>
+    /// 再生を行うかどうかのフラグを取得
+    /// </summary>
+    public bool GetMuteFlag()
+    {
+        return isMuteBgm;
+    }
+
+    /// <summary>
+    /// 再生を行うかどうかのフラグをセット
+    /// </summary>
+    /// <param name="set">現在のフラグを返す</param>
+    public void SetMuteFlag(bool set)
+    {
+        isMuteBgm = set;
+    }
+}
---
+++ BattaJump/Assets/Script/Audio/SeParameter.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// SE共通のパラメータ
+/// </summary>
+[CreateAssetMenu]
+public class SeParameter : ScriptableObject
+{
+    // SEの再生ボリューム
+    [SerializeField][Range(0,1)]
+    float playVolumeSe = 1;
+
+    // SEがミュートかどうか
+    [SerializeField]
+    bool isMuteSe = false;
+
+    /// <summary>
+    /// 再生ボリュームを取得
+    /// </summary>
+    /// <returns>現在のボリュームを返す</returns>
+    public float GetPlayVolume()
+    {
+        return playVolumeSe;
+    }
+
+    /// <summary>
+    /// 再生ボリュームをセット
+    /// </summary>
+    /// <param name="set">セットする値</param>
+    public void SetPlayVolume(float set)
+    {
+        playVolumeSe = set;
+    }
+
+    /// <summary>
+    /// 再生を行うかどうかのフラグを取得
+    /// </summary>
+    public bool GetMuteFlag()
+    {
+        return isMuteSe;
+    }
+
+    /// <summary>
+    /// 再生を行うかどうかのフラグをセット
+    /// </summary>
+    /// <param name="set">現在のフラグを返す</param>
+    public void SetMuteFlag(bool set)
+    {
+        isMuteSe = set;
+    }
+}
