828cc1e4e57b83256d313340810403bcac654669
tricore.as03 <tricore.as03@gmail.com>
2019-06-13 17:09:56 +0900

    Revert "該当箇所の修正とプロジェクトの設定変更"
    This reverts commit f46423890edbc33e2e89a8746d2b20e589023910.

--- BattaJump/Assets/Script/Audio/PlayingSeParentSwitcher.cs
+++ BattaJump/Assets/Script/Audio/PlayingSeParentSwitcher.cs
@@ -9,31 +9,22 @@ public class PlayingSeParentSwitcher : MonoBehaviour
 {
     // 再生が終了しているSEの親オブジェクトのトランスフォーム
     [SerializeField] Transform parentEndSe = default;
-    // 前フレームの子オブジェクト数
-    int prevChildCount = 0;
 
     /// <summary>
     /// 更新
     /// </summary>
     void Update()
     {
-        // 現在の子オブジェクトの数が前フレームよりも増えていれば
-        if (prevChildCount != transform.childCount)
+        for (int i = 0; i < transform.childCount; i++)
         {
-            for (int i = 0; i < transform.childCount; i++)
-            {
-                // 子オブジェクトを取得
-                GameObject childSe = transform.GetChild(i).gameObject;
+            // 子オブジェクトを取得
+            GameObject childSe = transform.GetChild(i).gameObject;
 
-                // 子オブジェクトの中で既に再生が終了しているSEは専用の親オブジェクトに切り替える
-                if (!childSe.activeSelf)
-                {
-                    childSe.transform.SetParent(parentEndSe);
-                }
+            // 子オブジェクトの中で既に再生が終了しているSEは専用の親オブジェクトに切り替える
+            if (!childSe.activeSelf)
+            {
+                childSe.transform.SetParent(parentEndSe);
             }
         }
-
-        // 現在の子オブジェクトの数を前フレームとして登録
-        prevChildCount = transform.childCount;
     }
 }
