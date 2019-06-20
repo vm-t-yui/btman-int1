6ca4c7a39935f017297758f339f3838c1eadf2fe
tricore.as03 <tricore.as03@gmail.com>
2019-06-20 18:56:17 +0900

    BGMの再生を制御するクラス「BgmPlayState」を作成した。
    ・ゲーム全体のBGMの制御を行うクラスを作成した。
    ・サウンドの再生を行うクラス「AudioPlayer」に指定のBGMが再生中かどうか判定する関数を追加した。
    ・BGMの再生と変更を行うクラス「BgmPlayState」を作成しました。
    ・サウンドのパラメータのロードのみを行うクラス「AudioParameterLoader」を作成した。
    ・アセットにBGMとSEを追加した。
    
    【確認ファイル】
    BgmPlayState.cs
    AudioPlayer.cs
    AudioParameterLoader.cs

---
+++ BattaJump/Assets/Script/Audio/AudioParameterLoader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// セーブされているオーディオをロードする
+/// </summary>
+public class AudioParameterLoader : MonoBehaviour
+{
+    /// <summary>
+    /// 起動処理
+    /// </summary>
+    void Awake()
+    {
+        // ロードする
+        AudioParameter.Load();
+    }
+}
