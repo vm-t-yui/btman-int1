da32f60662683e9a7963bbe9679b9c96a5c3a8ac
TanakaMasaki <0sv65728326399x@ezweb.ne.jp>
2019-07-01 12:22:02 +0900

    雲生成クラスの機能修正

--- BattaJump/Assets/Script/CloudCreater.cs
+++ BattaJump/Assets/Script/CloudCreater.cs
@@ -10,7 +10,11 @@ public class CloudCreater : MonoBehaviour
     [SerializeField]
     GameObject cloud = default;                                  // 複製する雲
 
+    List<GameObject> cloudClones = new List<GameObject>();       // 複製した雲のリスト
+
+    const int CloneNum = 5;                                      // 
     float interval = 0f;                                         // 生成する間隔
+    int useCount = 0;
 
     Vector3 createPos = Vector3.zero;                            // 生成時の位置
     Vector3 createScale = Vector3.zero;                          // 生成時のサイズ
@@ -33,15 +37,18 @@ public class CloudCreater : MonoBehaviour
     bool isAble = false;                                         // 処理許可フラグ
 
     /// <summary>
-    /// 起動時処理
+    /// 開始
     /// </summary>
-    void OnEnable()
+    void Start()
     {
-        // 1つ目の雲生成
-        Create();
-
-        // 処理を許可
-        isAble = true;
+        // 先に使う分だけ複製して非表示にしておく
+        for (int i = 0; i < CloneNum; i++)
+        {
+            GameObject newCloud = Instantiate(cloud, Vector3.zero, Quaternion.Euler(CreateRotation));
+            newCloud.transform.parent = transform;
+            newCloud.SetActive(false);
+            cloudClones.Add(newCloud);
+        }
     }
 
     /// <summary>
@@ -58,6 +65,33 @@ public class CloudCreater : MonoBehaviour
             // 雲を生成
             Create();
         }
+
+        // 画面外に行ったかチェック
+        CheckViewOut();
+    }
+
+    /// <summary>
+    /// 終了時処理
+    /// </summary>
+    void OnDisable()
+    {
+        // 全ての雲を非表示にする
+        for (int i = 0; i < CloneNum; i++)
+        {
+            cloudClones[i].SetActive(false);
+        }
+    }
+
+    /// <summary>
+    /// 生成開始処理
+    /// </summary>
+    public void StartCreate()
+    {
+        // 1つ目の雲生成
+        Create();
+
+        // 処理を許可
+        isAble = true;
     }
 
     /// <summary>
@@ -69,16 +103,40 @@ public class CloudCreater : MonoBehaviour
         createPos = new Vector3(Random.Range(CreateMinPos.x, CreateMaxPos.x), playerPos.position.y + Random.Range(CreateMinPos.y, CreateMaxPos.y), Random.Range(CreateMinPos.z, CreateMaxPos.z));
         createScale = new Vector3(Random.Range(3f, 5), Random.Range(3, 4f), Random.Range(1, 2));
 
-        // 指定された位置・回転角で複製
-        GameObject newCloud = Instantiate(cloud, createPos, Quaternion.Euler(CreateRotation));
+        // 指定された位置・サイズに更新
+        cloudClones[useCount].transform.position = createPos;
+        cloudClones[useCount].transform.localScale = createScale;
 
-        // 指定されたサイズに更新
-        newCloud.transform.localScale = createScale;
+        cloudClones[useCount].SetActive(true);
 
         // 次の生成間隔をランダムに決定
         interval = Random.Range(intervalMin, intervalMax);
 
         // 前の生成位置を更新
         prevCreatePos = playerPos.transform.position;
+
+        // 使用数をカウント
+        useCount++;
+
+        // 5個使用済みになったら使用数を0に戻す
+        if (useCount == 5)
+        {
+            useCount = 0;
+        }
+    }
+
+    /// <summary>
+    /// 画面外に行ったかチェック
+    /// </summary>
+    void CheckViewOut()
+    {
+        for (int i = 0; i < CloneNum; i++)
+        {
+            // 表示されていて画面外にあるなら非表示にする
+            if (cloudClones[i] && cloudClones[i].transform.position.y < playerPos.position.y - 10)
+            {
+                cloudClones[i].SetActive(false);
+            }
+        }
     }
 }
