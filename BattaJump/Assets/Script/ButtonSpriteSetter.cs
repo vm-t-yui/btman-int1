526d02cfe5127f615858748ec347d105c7c3f56a
tricore-05 <tricore.as05@gamil.com>
2019-07-02 20:09:02 +0900

    ボタンのスプライトをアトラスで設定

--- BattaJump/Assets/Script/ButtonSpriteSetter.cs
+++ BattaJump/Assets/Script/ButtonSpriteSetter.cs
@@ -25,6 +25,21 @@ public class ButtonSpriteSetter : MonoBehaviour
     Image settingButtonImage = default;        // 設定画面表示ボタンの画像
 
     [SerializeField]
+    Image titleLogo = default;                 // 設定画面木の看板の画像
+
+    [SerializeField]
+    Image[] volume = default;                 // 設定画面木の看板の画像
+
+    [SerializeField]
+    Image[] woodenButten = default;              // 設定画面木の看板の画像
+
+    [SerializeField]
+    Image[] woodenFrame = default;              // 設定画面木の看板の画像
+
+    [SerializeField]
+    Image[] woodenOutFrame = default;              // 設定画面木の看板の画像
+
+    [SerializeField]
     SpriteAtlas buttonAtlas;                   // ボタン用のスプライト
 
     /// <summary>
@@ -40,6 +55,31 @@ public class ButtonSpriteSetter : MonoBehaviour
         itemViewButtonImage.sprite = buttonAtlas.GetSprite("itemView");
 
         shareButtonImage.sprite = buttonAtlas.GetSprite("share");
+
+        titleLogo.sprite = buttonAtlas.GetSprite("vikingmaxxLogo");
+
+        foreach (var vol in volume)
+        {
+            vol.sprite = buttonAtlas.GetSprite("volume");
+        }
+
+        // 看板はある分だけセット
+        foreach (var sign in woodenButten)
+        {
+            sign.sprite = buttonAtlas.GetSprite("woodButton");
+        }
+
+        // フレームもある分だけセット
+        foreach (var frame in woodenFrame)
+        {
+            frame.sprite = buttonAtlas.GetSprite("woodenFrame");
+        }
+
+        // フレームもある分だけセット
+        foreach (var outFrame in woodenOutFrame)
+        {
+            outFrame.sprite = buttonAtlas.GetSprite("woodenOutFrame");
+        }
 
         // リザルトでは設定画面表示ボタンはないので、defaultじゃない場合のみスプライトをセット
         if (settingButtonImage != default)
