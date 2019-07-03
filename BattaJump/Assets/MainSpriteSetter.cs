fabd94ef79319f111309818d88609c22f4f1c973
tricore-05 <tricore.as05@gamil.com>
2019-07-03 11:50:09 +0900

    コメント修正

--- BattaJump/Assets/MainSpriteSetter.cs
+++ BattaJump/Assets/MainSpriteSetter.cs
@@ -4,19 +4,26 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.U2D;
 
+/// <summary>
+/// メインのスプライトクラス
+/// </summary>
 public class MainSpriteSetter : MonoBehaviour
 {
     [SerializeField]
-    Image gage = default;
+    Image gage = default;               //カウントダウンのゲージ
 
     [SerializeField]
-    Image gageZero = default;
+    Image gageZero = default;           //ゼロの状態のカウントダウンのゲージ
 
     [SerializeField]
-    SpriteAtlas canvasAtlas = default;
+    SpriteAtlas canvasAtlas = default;  //カンバス用スプライトアトラス
 
+    /// <summary>
+    /// 起動処理
+    /// </summary>
     void Awake()
     {
+        //ゲージのスプライト設定
         gage.sprite = canvasAtlas.GetSprite("timer");
 
         gageZero.sprite = canvasAtlas.GetSprite("timerZero");
