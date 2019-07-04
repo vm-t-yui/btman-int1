def2c2b2f93fb8cdf42e4367ab429ba65a952f1a
tricore-05 <tricore.as05@gamil.com>
2019-07-04 12:15:26 +0900

    アイテムボックスのNew表示関連スクリプト

---
+++ BattaJump/Assets/ItemBoxNewTextDisplay.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// アイテムボックスのNewのテキスト表示
+/// </summary>
+public class ItemBoxNewTextDisplay : MonoBehaviour
+{
+    [SerializeField]
+    ItemManager itemManager = default;  //アイテムマネージャー
+
+    [SerializeField]
+    GameObject newText = default;       //Newのテキスト
+
+    /// <summary>
+    /// 更新処理
+    /// </summary>
+    void Update()
+    {
+        //新しいアイテムを全て確認し終えたら
+        if(IsAllCheckNewItem())
+        {
+            //Newを消す
+            newText.SetActive(false);
+        }
+        else
+        {
+            newText.SetActive(true);
+        }
+    }
+
+    //新しいアイテムを全て確認し終えたかどうか
+    bool IsAllCheckNewItem()
+    {
+        int count = 0;  //確認カウント数
+
+        for (int i = 0; i < ItemManager.ItemNum; i++)
+        {
+            if(itemManager.GetIsNewText(i))
+			{
+                count++;
+			}
+        }
+
+        //カウント数がアイテム数と同数なら確認済
+        if(count == ItemManager.ItemNum)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
