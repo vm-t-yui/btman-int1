9776cb226ea5e075bc1d857ae2dae37f653d5ba6
tricore-05 <tricore.as05@gamil.com>
2019-07-12 15:02:24 +0900

    コメント忘れ

--- BtmanJump/Assets/Script/Ad/URLConnecter.cs
+++ BtmanJump/Assets/Script/Ad/URLConnecter.cs
@@ -3,29 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// 
+/// URLアクセスクラス
 /// </summary>
 public class URLConnecter : MonoBehaviour
 {
     [SerializeField]
-    string AndroidURL = default;
+    string AndroidURL = default;    //アンドロイド用URL
     [SerializeField]
-    string IosURL = default;
+    string IosURL = default;        //iOS用URL
 
-    /// <summary>
-    /// タンを押した時のメソッド
-    /// </summary>
-    public void PushLinkButton()
+	/// <summary>
+	/// ボタンを押した時のメソッド
+	/// </summary>
+	public void PushLinkButton()
     {
         string url =
 #if UNITY_ANDROID
-        AndroidURL;
+        AndroidURL;                 //アンドロイド用
 #elif UNITY_IOS
-        IosURL;
+        IosURL;                     //iOS用
 #else
-        "unexpected_platform";
+		"unexpected_platform";      //それ以外
 #endif
 
+        //URLにアクセス
         Application.OpenURL(url);
     }
 }
