ab5bb93114a6e9a69cd1d6be0d86d43031442062
tricore-05 <tricore.as05@gamil.com>
2019-07-12 14:56:48 +0900

    自社広告(おすすめアプリ)追加

---
+++ BtmanJump/Assets/Script/Ad/URLConnecter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+public class URLConnecter : MonoBehaviour
+{
+    [SerializeField]
+    string AndroidURL = default;
+    [SerializeField]
+    string IosURL = default;
+
+    /// <summary>
+    /// タンを押した時のメソッド
+    /// </summary>
+    public void PushLinkButton()
+    {
+        string url =
+#if UNITY_ANDROID
+        AndroidURL;
+#elif UNITY_IOS
+        IosURL;
+#else
+        "unexpected_platform";
+#endif
+
+        Application.OpenURL(url);
+    }
+}
